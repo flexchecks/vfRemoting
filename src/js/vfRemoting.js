e0ff80fcf1a7187f95c16e132e8b3df905f65753
Zachary Trank-Zelewicz <trank.zachary@gmail.com>
2016-11-05 15:27:22 -0400

    Adds Changes I made to the wrong file
    Dumb me. See previous commits for changes

--- src/js/vfRemoting.js
+++ src/js/vfRemoting.js
@@ -1,30 +1,46 @@
 /*global*/
+/*
+MIT License
+
+Copyright (c) 2016 Zachary C. Trank-Zelewicz
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 'use strict';
 const RemoteAction = function(o) {
-	const doBeforeSend = [];
-	const doOnSuccess = [];
-	const doOnFailure = [];
-	const doOnComplete = [];
+	const doBeforeSend = [];	// Array of functions to call before invoking Manager.invokeAction
+	const doOnSuccess = [];		// Array of functions to call during the remote action callback when the response is a success
+	const doOnFailure = [];		// Array of functions to call during the remote action callback when the response is a failure
+	const doOnComplete = [];	// Array of functions to call during the remote action callback regardless if it is a success or failure
 
-	var Manager;
+	var Manager;				// The manager, defaults to Visualforce.remoting.Manager
 	try {
+		// Try to set the manager, if it fails, then the remoting manager is not present on the page.
 		Manager = Visualforce.remoting.Manager;
 	} catch(e) {
-
+		console.log('No Remoting Manager found');
 	}
-
-	var fire = function(arry) {
-		arry = Array.isArray(arry) ? arry : [];
-		var args = Array.prototype.slice.call(arguments, 1);
-		for(let i = 0; i < arry.length; i++) {
-			if(typeof arry[i] === 'function')
-				arry[i].apply(arry[i], args);
-		}
-	};
 	
-
+	// Set the options object if it is an object.
 	const options = typeof o === 'object' ? o : undefined;
 
+	// Logic to bind a funciton to one of the arrays. This will only add objects where typeof  is a Function
 	var addFunction = function(arry) {
 		var args = Array.from(arguments)[1];
 		for(let i = 0; i < args.length; i++) {
@@ -34,6 +50,31 @@ const RemoteAction = function(o) {
 		
 	};
 
+	// Logic to find and unbind a named function from one of the arrays. If no name is provided, all functions are unbound.
+	var removeFunction = function(arry, fnName) {
+		if(fnName === undefined) {
+			arry.splice(0, arry.length);
+		}
+
+		for(let i = 0; i < arry.length; i++) {
+			if(arry[i].name === fnName)
+				arry.splice(i, 1);
+		}
+
+		return arry;
+	};
+
+	// Fires each function found in one of the binding arrays
+	var fire = function(arry) {
+		arry = Array.isArray(arry) ? arry : [];
+		var args = Array.prototype.slice.call(arguments, 1);
+		for(let i = 0; i < arry.length; i++) {
+			if(typeof arry[i] === 'function')
+				arry[i].apply(arry[i], args);
+		}
+	};
+	
+	/* ----------------------------------------- Getters ----------------------------------------- */
 	this.getPrior = function() {
 		return doBeforeSend;
 	};
@@ -54,19 +95,7 @@ const RemoteAction = function(o) {
 		return options;
 	};
 
-	var removeFunction = function(arry, fnName) {
-		if(fnName === undefined) {
-			arry.splice(0, arry.length);
-		}
-
-		for(let i = 0; i < arry.length; i++) {
-			if(arry[i].name === fnName)
-				arry.splice(i, 1);
-		}
-
-		return arry;
-	};
-
+	/* ------------------------------------ Binding Functions ------------------------------------ */
 	this.onPrior = function() {
 		addFunction(doBeforeSend, arguments);
 		return this;
@@ -87,6 +116,7 @@ const RemoteAction = function(o) {
 		return this;
 	};
 
+	/* ----------------------------------- Unbinding Functions ----------------------------------- */
 	this.offPrior = function(fnName) {
 		removeFunction(doBeforeSend, fnName);
 		return this;
@@ -107,17 +137,28 @@ const RemoteAction = function(o) {
 		return this;
 	};
 
+	/* ------------------------------------- Set the Manager ------------------------------------- */
 	this.setManager = function(mng) {
 		Manager = mng;
 		return this;
 	};
 
+	/* -------------------------------- Invoke the Remote Action  -------------------------------- */
 	this.send = function(remoteAction) {
+		// Make sure there is a manager
 		if(Manager === undefined)
 			throw new Error('Visualforce Manager not defined');
 
+		// Make sure the remoteAction is a string
 		if(remoteAction === undefined || typeof remoteAction !== 'string')
 			throw new Error('Method must be a string');
+
+		/* 
+		 * Make sure the remote action fits the requirements of:
+		 * Namespace.Controller.Method
+		 * or
+		 * Controller.Method
+		 */
 		var parts = remoteAction.split('.');
 		var namespace, controller, method;
 
@@ -132,76 +173,78 @@ const RemoteAction = function(o) {
 			throw new Error('Invalid remote action supplied: ' + remoteAction);
 		}
 
+		// Get all the other parameters and add them to a list
 		var args = Array.prototype.slice.call(arguments, 1);
 
 		args = args === undefined ? [] : args;
 
+		// Callback for the remote action
 		var cb = function(vfResponse, vfEvent) {
+			// Check if the status is Truthy, and isSuccess is either true or undefined
 			if(vfEvent.status && (vfResponse.isSuccess === undefined || vfResponse.isSuccess === true)) {
+				// Fire all onSuccess bound functions
 				fire(doOnSuccess, vfResponse, vfEvent, this);
-				/*for(let i = 0; i < doOnSuccess.length; i++) {
-					if(typeof doOnSuccess[i])
-					doOnSuccess[i](vfResponse, vfEvent, this);
-				}*/
 			} else {
+				// Create an error object
 				var err = new Error(vfEvent.message);
 				err.result = vfResponse;
 				if(vfEvent.type === 'exception') {
 					err.apexStackTrace = vfEvent.where;
 				}
-				fire(doOnFailure, vfResponse, err, this);
-				/*
-				for(let i = 0; i < doOnFailure.length; i++) {
-					doOnFailure[i](err, vfResponse, this);
-				}
-				*/
+				// Fire all onFailure bound functions
+				fire(doOnFailure, vfResponse, vfEvent, err, this);
 			}
+
+			// Fire all onComplete bound functions
 			fire(doOnComplete, vfResponse, vfEvent, this);
-			/*
-			for(let i = 0; i < doOnComplete.length; i++) {
-				doOnComplete[i](this);
-			}
-			*/
 		};
 
+		// Add the callback to the list of parameters
 		args.push(cb);
 
+		// If options is defined, add that to the list of parameters
 		if(options !== undefined) {
 			args.push(options);
 		}
 
+		// Put the remote action in the front of the list
 		args.splice(0,0,remoteAction);
 
-		/*
-		for(let i = 0; i < doBeforeSend.length; i++) {
-			doBeforeSend[i](this);
-		}
-		*/
+		// Fire all onPrior bound functions
 		fire(doBeforeSend, this);
 
+		// Invoke the remote action
 		Manager.invokeAction.apply(Manager, args);
 	};
 };
 
 
 const RemotePromise = function(o) {
+
+	// Create a new instance of RemoteAction
 	const ra = new RemoteAction(o);
+
+	// Returns the RemoteAction object to expose its functions
 	this.getRemoteAction = function() {
 		return ra;
 	};
+
+	// Returns a promise object of the RemoteAction
 	this.send = function(method, data) {
 		return new Promise(function(resolve, reject) {
+			// Bind a function to onSuccess to resolve the promise, and onFailure to reject the promise, then send the request.
 			ra
-				.onSuccess(function doResolve(vfResponse, vfEvent) {
-					resolve({response: vfResponse, event : vfEvent});
+				.onSuccess(function doResolve(vfResponse, vfEvent, remoteAction) {
+					resolve({response: vfResponse, event : vfEvent, remoteAction : remoteAction});
 				})
-				.onFailure(function doReject(err) {
-					reject(err);
-				});
-			ra.send(method, data);
+				.onFailure(function doReject(vfResponse, vfEvent, err, remoteAction) {
+					reject({response: vfResponse, event : vfEvent, remoteAction : remoteAction, error: err});
+				})
+				.send(method, data);
 		});
 	};
 };
+
 try {
 	exports.RemoteAction = RemoteAction;
 	exports.RemotePromise = RemotePromise;
